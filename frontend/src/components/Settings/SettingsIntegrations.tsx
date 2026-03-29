3b2782422e2bc02a1a89b043a00a686d05d9b4d3
Denis Dvali <2803994+denho@users.noreply.github.com>
2026-03-29 18:45:40 +0400

    - Add support for installing Faved as a PWA app - Optimize logo and favicon images

--- frontend/src/components/Settings/SettingsIntegrations.tsx
+++ frontend/src/components/Settings/SettingsIntegrations.tsx
@@ -1,9 +1,11 @@
 import { observer } from 'mobx-react-lite';
+import { InstallApp } from '@/components/Settings/Integrations/InstallApp.tsx';
 import { AppleShortcut } from '@/components/Settings/Integrations/AppleShortcut.tsx';
 import { Bookmarklet } from '@/components/Settings/Integrations/Bookmarklet.tsx';
 export const SettingsIntegrations = observer(({ onSuccess, source }: { onSuccess?: () => void; source: string }) => {
   return (
-    <div className="mx-auto w-full space-y-6">
+    <div className="mx-auto w-full space-y-4">
+      <InstallApp />
       <Bookmarklet onSuccess={onSuccess} />
       <AppleShortcut onSuccess={onSuccess} source={source} />
     </div>
